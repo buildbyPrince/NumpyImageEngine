bafdb3132a7d43ac93b15b352000dd04c650d80c
user.email <theprince9627@gmail.com>
2026-08-15 21:55:04 +0530

    getMetaData ready in imageEngine.py

--- imageEngine.py
+++ imageEngine.py
@@ -3,11 +3,21 @@ import matplotlib.pyplot as plt
 
 class ImageEngine : 
     def __init__(self, image_path : str):
-        self.imgArray = plt.imread('assets/sample.jpg')
+        self.imgArray = plt.imread(image_path)
         if not isinstance(self.imgArray, np.ndarray):
             raise TypeError("Loaded image is not a valid NumPy array")
         
     def getMetaData(self) -> dict :
-        pass
+        height , width = self.imgArray.shape[:2]
+        metadata = {
+            "shape" : self.imgArray.shape,
+            "dtype" : self.imgArray.dtype,
+            "min_val" : self.imgArray.min(),
+            "max_val" : self.imgArray.max(),
+            "total_pixel" : height * width
+        }
+        return metadata
     def saveImage(self, output_path : str, arr : np.ndarray = None) : 
         pass
+o = ImageEngine('assets/sample.jpg')
+print(o.getMetaData())
