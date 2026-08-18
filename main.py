a5337d3594563c718f6581486389eaf40b736f33
user.email <theprince9627@gmail.com>
2026-08-18 15:02:54 +0530

    main.py ready to use

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import imageEngine as imgEn
 
 obj = imgEn.ImageEngine('assets/sample.jpg')
-print(obj.to_grayScale())
+
+print(obj.getMetaData())
+obj.saveImage("assets/gray.jpg", obj.to_grayScale())
+obj.saveImage("assets/ext.jpg", obj.extractChannel('G'))
+obj.saveImage("assets/flipV.jpg", obj.flip('vertical'))
+obj.saveImage("assets/flipH.jpg", obj.flip('Horizontal'))
+obj.saveImage("assets/rotclkw.jpg", obj.rotate90('clockwise'))
+obj.saveImage("assets/rotaclkw.jpg", obj.rotate90('counterclockwise'))
+obj.saveImage("assets/adj.jpg", obj.adjBriCT(70, 1.1))
